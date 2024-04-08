92b93bd564e28296f844a7f19d05bdce40d9171a
Evan Doyle <evanmdoyle@gmail.com>
2024-04-08 15:43:05 -0700

    bridge init render

--- render-start.sh
+++ render-start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-gunicorn public-testing.wsgi:application -w "${WEB_CONCURRENCY:-4}" -k uvicorn.workers.UvicornWorker
+gunicorn public-testing.wsgi:application -w "${WEB_CONCURRENCY:-4}"
